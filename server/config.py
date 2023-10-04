202b07a39a69e06b6844fe60140086769f63405b
Ron Maina <ronmwangi00@gmail.com>
2023-10-05 00:53:21 +0300

    Updated config.py

--- server/config.py
+++ server/config.py
@@ -13,12 +13,6 @@ app = Flask(
     template_folder='../client/build'
 )
 
-app.secret_key = ' '
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chatwave.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SESSION_TYPE'] = 'filesystem'
-app.json.compact = False
-
 metadata = MetaData(naming_convention={
     "ix": 'ix_%(column_0_label)s',
     "uq": "uq_%(table_name)s_%(column_0_name)s",
