da7cf39ede49792fc4789549d20d043bd7fdb3f0
Ron Maina <ronmwangi00@gmail.com>
2023-10-05 18:31:45 +0300

    Push content functionality

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -12,82 +12,95 @@ import {
 import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 
-function Sidebar() {
+function Sidebar({onchange}) {
+    const navigate = useNavigate()
+    const [isActive, setIsActive] = useState(false);
+
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
     const handleSidebarOpen = () => {
       setSidebarOpen(true);
+      handleOver()
     };
-  
+
     const handleSidebarClose = () => {
       setSidebarOpen(false);
+      handleOut()
     };
     
-    const navigate = useNavigate()
+    function handleOver(){
+      setIsActive(true)
+    }
+    onchange(isActive)
+
+    function handleOut(){
+        setIsActive(false)
+    }
+    onchange(isActive)
 
     function Logout(){
         navigate("/", {replace: true})
     }
 
     return (
-        <div style={{marginLeft: '-20px'}} onMouseLeave={handleSidebarClose}>
-            <div id='sidebar'>
-                {!sidebarOpen ? (
-                    <>
-                        <CDBSidebarHeader onMouseEnter={handleSidebarOpen}>
-                            <i className="fa fa-user fa-large"></i>
-                        </CDBSidebarHeader>
-                        <CDBSidebarContent className="sidebar-content">
-                            <CDBSidebarMenu id = 'sidebar-menu'>
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="toggle-on"></CDBSidebarMenuItem>
+        <div id='sidebar' onMouseLeave={handleSidebarClose}>
+            {!sidebarOpen ? (
+                <>
+                    <CDBSidebarHeader onMouseEnter={handleSidebarOpen} 
+                    onMouseLeave={handleOut}>
+                        <i className={"fa fa-user fa-large"}></i>
+                    </CDBSidebarHeader>
+                    <CDBSidebarContent className="sidebar-content">
+                        <CDBSidebarMenu id = 'sidebar-menu'>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="toggle-on"></CDBSidebarMenuItem>
 
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="home"></CDBSidebarMenuItem>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="home"></CDBSidebarMenuItem>
 
-                                <NavLink to="/contacts">
-                                    <CDBSidebarMenuItem id= 'sidebar-items' icon="address-book"></CDBSidebarMenuItem>
-                                </NavLink>
-                                
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="plus-circle"></CDBSidebarMenuItem>
+                            <NavLink to="/contacts">
+                                <CDBSidebarMenuItem id= 'sidebar-items' icon="address-book"></CDBSidebarMenuItem>
+                            </NavLink>
                             
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="times" onClick={Logout}></CDBSidebarMenuItem>
-                            </CDBSidebarMenu>
-                        </CDBSidebarContent>
-                        <CDBSidebarFooter style={{ textAlign: 'center' }}>
-                            <div className="sidebar-btn-wrapper" style={{padding: '250px 5px',}}>
-                                <i i class="fa fa-cog"></i>
-                            </div>
-                        </CDBSidebarFooter>
-                    </>
-                ) : (
-                    <>
-                        <CDBSidebarHeader>
-                            <i className="fa fa-user fa-large"> Profile</i> 
-                        </CDBSidebarHeader>
-                        <CDBSidebarContent className="sidebar-content">
-                            <CDBSidebarMenu id = 'sidebar-menu'>
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="toggle-on">Theme</CDBSidebarMenuItem>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="plus-circle"></CDBSidebarMenuItem>
+                        
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="times" onClick={Logout}></CDBSidebarMenuItem>
+                        </CDBSidebarMenu>
+                    </CDBSidebarContent>
+                    <CDBSidebarFooter style={{ textAlign: 'center' }}>
+                        <div className="sidebar-btn-wrapper" style={{padding: '250px 5px',}}>
+                            <i i class="fa fa-cog"></i>
+                        </div>
+                    </CDBSidebarFooter>
+                </>
+            ) : (
+                <>
+                    <CDBSidebarHeader>
+                        <i className="fa fa-user fa-large"> Profile</i> 
+                    </CDBSidebarHeader>
+                    <CDBSidebarContent className="sidebar-content">
+                        <CDBSidebarMenu id = 'sidebar-menu'>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="toggle-on">Theme</CDBSidebarMenuItem>
 
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="home">Home</CDBSidebarMenuItem>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="home">Home</CDBSidebarMenuItem>
 
-                                <NavLink to="/contacts">
-                                    <CDBSidebarMenuItem id= 'sidebar-items' icon="address-book">Contacts</CDBSidebarMenuItem>
-                                </NavLink>
+                            <NavLink to="/contacts">
+                                <CDBSidebarMenuItem id= 'sidebar-items' icon="address-book">Contacts</CDBSidebarMenuItem>
+                            </NavLink>
 
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="plus-circle">Add Contacts</CDBSidebarMenuItem>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="plus-circle">Add Contacts</CDBSidebarMenuItem>
 
-                                <CDBSidebarMenuItem id= 'sidebar-items' icon="times" onClick={Logout}>Logout</CDBSidebarMenuItem>
-                            </CDBSidebarMenu>
-                        </CDBSidebarContent>
+                            <CDBSidebarMenuItem id= 'sidebar-items' icon="times" onClick={Logout}>Logout</CDBSidebarMenuItem>
+                        </CDBSidebarMenu>
+                    </CDBSidebarContent>
 
-                        <CDBSidebarFooter style={{ textAlign: 'center' }}>
-                            <div className="sidebar-btn-wrapper" style={{padding: '250px 5px',}}>
-                                <i i class="fa fa-cog"> Settings</i>
-                            </div>
-                        </CDBSidebarFooter>
-                    </>
-                )}
-            </div>
+                    <CDBSidebarFooter style={{ textAlign: 'center' }}>
+                        <div className="sidebar-btn-wrapper" style={{padding: '250px 5px',}}>
+                            <i i class="fa fa-cog"> Settings</i>
+                        </div>
+                    </CDBSidebarFooter>
+                </>
+            )}
         </div>
+        
     );
     
     
