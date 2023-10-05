d5386911a263a53f687e8649792aec13f910dc54
Ron Maina <ronmwangi00@gmail.com>
2023-10-06 01:11:30 +0300

    Home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,13 +12,23 @@ function Home({user}) {
       }
     }
   return (
-      <div className='home-screen' style={{ display: 'flex' }}>
-        <Sidebar onchange={renderChange}/>
-        <div id={isActive ? 'slide-out' : 'slide-in'}>
-          <h2 className='headings-light'>Chats</h2>
-        </div>
-       
+    <div className='home-screen'>
+      <Sidebar onchange={renderChange}/>
+      <div className={isActive ? 'slide-out' : 'slide-in'} style={{overflowY: 'auto'}}>
+          <h2 className='headings-light'>ChatWave</h2>
+          <div>
+            <h4 style={{marginTop: '150px'}}>Chats</h4>
+            <div id='profile'>
+              <img className= 'image' src={user.profile_pic} alt='profile_pic'/>
+              <p style={{margin: '10px'}}>{user.name}</p>
+            </div>
+            <div style={{color: 'red', right: '70px', position: 'absolute'}}>
+              <i class="fa fa-trash"></i>
+            </div>
+          </div>
+          
       </div>
+    </div>
   )
 }
 
