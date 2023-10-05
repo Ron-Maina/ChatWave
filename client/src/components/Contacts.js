d9304b8171736d4063ecced0cfff110b6a952cf3
Ron Maina <ronmwangi00@gmail.com>
2023-10-05 22:05:10 +0300

    Delete and navigate to conversation page functionality

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from 'react'
+import {useNavigate } from 'react-router-dom'
 import Sidebar from './Sidebar'
 
 
-function Contacts() {
+function Contacts({onChat}) {
+    const navigate = useNavigate()
+
     const [contacts, setContacts] = useState([])
 
     useEffect(() => {
         fetch('/contacts')
         .then(res => res.json())
         .then(data => setContacts(data))
-    }, [])
+    }, [contacts])
     const [isActive, setIsActive] = useState(false);
 
     function renderChange(state){
@@ -20,11 +23,23 @@ function Contacts() {
         }
     }
 
-    function handleClick(){
-        console.log('hi')
+    function handleClick(contact){
+        onChat(contact)
+        navigate('/chat')
+
     }
-    function handleDelete(){
-        return
+    
+    function handleDelete(id){
+        fetch(`/contacts/${id}`, {
+            method: 'DELETE',   
+        })
+        .then(res => {
+            if (res.ok){
+                alert('Deleted successfully')
+            }
+        })
+        let new_contacts = contacts.filter(contact => contact.id !== id)
+        setContacts(new_contacts)
     }
    
     return (
@@ -35,13 +50,16 @@ function Contacts() {
                 <div className='info' >
                     {contacts.map(contact => (
                         <div key={contact.id} 
-                        style={{display: 'flex', gap: '30px', margin: '20px'}}
-                        onClick={handleClick}>
-                            <img style= {{height: '50px', width: '50px', borderRadius: '50%'}}src={contact.profile_pic} alt='profile_pic'/>
-                            <p style={{margin: '10px'}}>{contact.name}</p>
+                        style={{display: 'flex', gap: '30px', margin: '20px'}}>
+
+                            <div style={{display: 'flex', gap: '30px'}} onClick={() => handleClick(contact)}>
+                                <img style= {{height: '50px', width: '50px', borderRadius: '50%'}}src={contact.profile_pic} alt='profile_pic'/>
+                                <p style={{margin: '10px'}}>{contact.name}</p>
+                            </div>
+
                             <div 
                             style={{color: 'red', right: '70px', position: 'absolute', marginTop: '10px'}}
-                            onClick={handleDelete}>
+                            onClick={() => handleDelete(contact.id)}>
                                 <i class="fa fa-trash"></i>
                             </div>
                         </div>
