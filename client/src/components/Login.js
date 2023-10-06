1b7c38b056a1326083cf129cf82aa1d740ae46d6
Ron Maina <ronmwangi00@gmail.com>
2023-10-06 11:07:40 +0300

    Updated login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,71 @@
 import React from 'react'
+import { useState} from 'react'
+import {useNavigate } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import {BsFillChatDotsFill} from "react-icons/bs";
 
-function Login() {
+
+
+function Login({onLogin}) {
+    const navigate = useNavigate() 
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPass] = useState("")
+
+    function handleSubmit(e){
+        e.preventDefault()
+        const userDetails = {
+            "email": Email,
+            "password": Password,
+        }
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userDetails),
+        })
+        .then(res => res.json())
+        .then(user => onLogin(user))
+        navigate("/home", {replace: true})
+    }
+
   return (
-    <div>
-        <h1 style={{color: "white"}}>Hi</h1>
+    <div className='frame' id='authenticate'>
+        <div style={{textAlign: 'center'}}>
+            <h2 id='signup-text'>ChatWave</h2>
+            <div style={{color: 'white', fontSize: '40px'}}><BsFillChatDotsFill/></div>
+        </div>
+
+        <form onSubmit={handleSubmit} style={{ marginTop: "30%"}}>
+            <label htmlFor='email' className='input-label'>Email: </label>
+            <br/>
+            <input
+            className='input-field'
+            name='email'
+            required
+            type='text'
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <br/>
+            
+            <label htmlFor='password' className='input-label' style={{marginTop: '20px'}}>Password: </label>
+            <br/>
+            <input
+            className='input-field'
+            autoComplete="off"
+            name='password'
+            required
+            type='text'
+            value={Password}
+            onChange={(e) => setPass(e.target.value)}
+            />
+
+            <div style={{marginTop: '20px'}}>
+                <Button variant="info" type='submit'>Login</Button>{' '}   
+            </div>
+        </form>
     </div>
   )
 }
