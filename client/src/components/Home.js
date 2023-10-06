227a08b2e8d9dc6c81920cfcb19de72c7b6e50c0
Ron Maina <ronmwangi00@gmail.com>
2023-10-07 02:26:56 +0300

    Updated rendered information

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react'
 import Sidebar from './Sidebar'
 import Search from './Search';
+import {useNavigate } from 'react-router-dom'
 
-function Home({chatContact, user}) {
+function Home({conversations, user, onChat}) {
     const [isActive, setIsActive] = useState(false);
     const [searchTerm, setSearchTerm] = useState("");
     
-    
+    const navigate = useNavigate()
+
     function renderChange(state){
       if (state === true){
         setIsActive(true);
@@ -15,9 +17,24 @@ function Home({chatContact, user}) {
       }
     }
 
-    const filtered_chats = chatContact.filter(chat => {
+    const filtered_chats = conversations.filter(chat => {
       return chat.name.toLowerCase().includes(searchTerm.toLowerCase())
     })
+
+    function handleDelete(id){
+      fetch(`/chats/${id}`,{
+        method: 'DELETE'
+      })
+      .then(res => {
+        if (res.ok){
+            alert('Deleted successfully')
+        }
+      })
+    }
+    function handleClick(contact){
+      onChat(contact)
+      navigate("/chat")
+    }
 
   return (
     <div className='home-screen'>
@@ -29,10 +46,10 @@ function Home({chatContact, user}) {
           <div className='info'>
             <h5 className='headings-light' style={{fontSize: '25px'}}>Chats</h5>
             {filtered_chats.map(contact => (
-              <div key={contact.id} id='profile' style={{marginLeft: '15px', marginTop: '15px'}}>
+              <div key={contact.id} id='profile' style={{marginLeft: '15px', marginTop: '15px'}} onClick={() => handleClick(contact)}>
                 <img className= 'image' src={contact.profile_pic} alt='profile_pic'/>
                 <p style={{margin: '15px'}}>{contact?.name}</p>
-                <i style={{color: 'red', position: 'absolute', right: '30px', marginTop: '17px'}} class="fa fa-trash"></i>
+                <i onClick={() => handleDelete(contact.id)} style={{color: 'red', position: 'absolute', right: '30px', marginTop: '17px'}} class="fa fa-trash"></i>
               </div>
               
             ))}
