5ed2fd12e73edab78f5b2232fda327a9aa67355f
Ron Maina <ronmwangi00@gmail.com>
2023-10-06 19:09:33 +0300

    Updated Login routes

--- server/app.py
+++ server/app.py
@@ -3,16 +3,16 @@ from flask_restful import Resource
 from werkzeug.exceptions import BadRequest
 
 
-from config import app, db, api
+from config import app, db, api, bcrypt
 from models import  Users, Contacts, Chats
 
 app.config['SESSION_TYPE'] = 'filesystem'
 
 
-@app.route('/signup')
+@app.route('/')
 def index():
     return render_template("index.html")
-    
+   
 class Signup(Resource):
     def post(self):
         try:
@@ -20,14 +20,15 @@ class Signup(Resource):
             new_user = Users(
                 name = data.get('name'),
                 email = data.get('email'),
-                number = data.get('number')
+                number = data.get('number'),
+                profile_pic = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaiKiPcLJj7ufrj6M2KaPwyCT4lDSFA5oog&usqp=CAU'
             )
             new_user.password_hash = data.get('password')
 
             db.session.add(new_user)
             db.session.commit()
 
-            session['user'] = new_user.id
+            session['user_id'] = new_user.id
 
             user_dict = {
                 "id": new_user.id,
@@ -47,19 +48,20 @@ class Signup(Resource):
 class Login(Resource):
     def post(self):
         user = Users.query.filter(Users.email == request.get_json()['email']).first()
-        if user:
-            session['user_id'] = user.id
+        password = request.get_json()['password']
+        if (user) and (user.authenticate(password) == True):
+            session['user'] = user.id
             response = make_response(
                 jsonify(user.to_dict()),
                 200
             )
             return response
         else:
-            return jsonify({"message":"You do not have an account"})
+            return {"message":"You do not have an account"}, 401
     
 class Logout(Resource):
     def delete(self):
-        session["user_id"] = None
+        session["user"] = None
         return {}, 204
     
 class CheckSession(Resource):
@@ -88,6 +90,18 @@ class Contact(Resource):
             200
         )
         return result
+    
+    def post(self):
+        data = request.get_json()
+        if data:
+            new_contact = Contacts(
+                name = data.get('name'),
+                email = data.get('email'),
+                number = data.get('number'),
+                profile_pic = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaiKiPcLJj7ufrj6M2KaPwyCT4lDSFA5oog&usqp=CAU'   
+            )
+            db.session.add(new_contact)
+            db.session.commit()
     
 class GetContactByID(Resource):
     def get(self, id):
@@ -124,30 +138,88 @@ class GetContactByID(Resource):
         )
 
         return response
-        
+    
+class GetUserByID(Resource):
+    def patch(self, id):
+        user = Users.query.filter_by(id=id).first()
+        data = request.get_json()
+        if user:
+            try:
+                if 'name' in data:
+                    user.name = data.get('name')
+                if 'email' in data:
+                    user.email = data.get('email')
+                if 'number' in data:
+                    user.number = data.get('number')
+                if 'profile_pic' in data:
+                    user.profile_pic = data.get('profile_pic')
+
+                
+                db.session.commit()
+                return jsonify(user.to_dict())
+               
+            except Exception as e:
+                db.session.rollback()  
+                response = make_response(
+                    jsonify({"error": str(e)}),
+                    500
+                )
+        else:
+            response = make_response(
+                jsonify({"error": "User not found"}),
+                404
+            )
+
+        return response
+
+
 class Chat(Resource):
     def post(self):
         data = request.get_json()
-        if data:
-            message = Chats(
-                messages = data.get('message'),
-                responses = 'figure out later',
-                contact_id = 'jfn',
-                user_id = session.get('user')
+        user = session.get('user_id')
+        if user:
+            try:
+                message = Chats(
+                    messages = data.get('message'),
+                    responses = 'figure out later',
+                    user_id = user,
+                    contact_id = data.get('contact_id')   
+                )
+                db.session.add(message)
+                db.session.commit()
+
+            except Exception as e:
+                db.session.rollback()  
+                response = make_response(
+                    jsonify({"error": str(e)}),
+                    500
+                )
+        else:
+            response = make_response(
+                jsonify({"error": "User not found"}),
+                404
             )
-            db.session.add(message)
-            db.session.commit()
+
+            return response
+        
+class ContactSession(Resource):
+    def get(self, id):
+        user = Users.query.filter_by(id=id).first()
+        return jsonify(user.to_dict())
     
+
 api.add_resource(Signup, '/signup')
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
 api.add_resource(CheckSession, '/check_session')
 api.add_resource(Contact, '/contacts')
 api.add_resource(GetContactByID, '/contacts/<int:id>')
+api.add_resource(GetUserByID, '/users/<int:id>')
 api.add_resource(Chat, '/chats')
-
+api.add_resource(ContactSession, '/contact-session/<int:id>')
 
 
 
 if __name__ == '__main__':
+    db.session.creat_all()
     app.run(debug = True)
