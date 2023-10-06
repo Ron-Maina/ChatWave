d7fcc4bdd67685e5a11740ed95f19eb749866fad
Ron Maina <ronmwangi00@gmail.com>
2023-10-06 19:15:03 +0300

    Texted contacts can appear here

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react'
 import Sidebar from './Sidebar'
+import Search from './Search';
 
-function Home({user}) {
+function Home({chatContact, user}) {
     const [isActive, setIsActive] = useState(false);
+    const [searchTerm, setSearchTerm] = useState("");
+    
     
     function renderChange(state){
       if (state === true){
@@ -11,20 +14,30 @@ function Home({user}) {
         setIsActive(false)
       }
     }
+
+    const filtered_chats = chatContact.filter(chat => {
+      return chat.name.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+
   return (
     <div className='home-screen'>
-      <Sidebar onchange={renderChange}/>
+      <Sidebar onchange={renderChange} user={user}/>
       <div className={isActive ? 'slide-out' : 'slide-in'} style={{overflowY: 'auto'}}>
           <h2 className='headings-light'>ChatWave</h2>
-          <div>
-            <h4 style={{marginTop: '150px'}}>Chats</h4>
-            <div id='profile'>
-              <img className= 'image' src={user.profile_pic} alt='profile_pic'/>
-              <p style={{margin: '10px'}}>{user.name}</p>
-            </div>
-            <div style={{color: 'red', right: '70px', position: 'absolute'}}>
-              <i class="fa fa-trash"></i>
-            </div>
+          <Search searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+
+          <div className='info'>
+            <h5 className='headings-light' style={{fontSize: '25px'}}>Chats</h5>
+            {filtered_chats.map(contact => (
+              <div key={contact.id} id='profile' style={{marginLeft: '15px', marginTop: '15px'}}>
+                <img className= 'image' src={contact.profile_pic} alt='profile_pic'/>
+                <p style={{margin: '15px'}}>{contact?.name}</p>
+                <i style={{color: 'red', position: 'absolute', right: '30px', marginTop: '17px'}} class="fa fa-trash"></i>
+              </div>
+              
+            ))}
+            
+            
           </div>
           
       </div>
