34efb62be11fb3bb4fa62939ae35ccfd321e5c70
Ron Maina <ronmwangi00@gmail.com>
2023-10-08 15:24:13 +0300

    Updated versions

---
+++ server/migrations/versions/f41f76804524_data_check.py
@@ -0,0 +1,24 @@
+"""Data check
+
+Revision ID: f41f76804524
+Revises: 73a3d24dcc04
+Create Date: 2023-10-08 15:00:01.045571
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f41f76804524'
+down_revision = '73a3d24dcc04'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
